31f2a6e8b36cfb53731d48097e3f300d76453278
Michal Borejszo <michael.borejszo@gmail.com>
2020-05-15 23:26:34 +0200

    add CLI tests stub

---
+++ tests/cli-tests.rs
@@ -0,0 +1,34 @@
+use std::process::Command;  // Run programs
+use assert_cmd::prelude::*; // Add methods on commands
+use predicates::prelude::*; // Used for writing assertions
+use tempfile::NamedTempFile;
+use std::io::Write;
+use std::fs;
+
+#[test]
+fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
+    let mut cmd = Command::cargo_bin("rnc")?;
+    cmd.arg("--dos2unix")
+        .arg("test/file/doesnt/exist");
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("The system cannot find the path specified"));
+
+    Ok(())
+}
+
+#[test]
+fn find_content_in_file() -> Result<(), Box<dyn std::error::Error>> {
+    let mut file = NamedTempFile::new()?;
+    write!(file, "foo\r\nbar\r\n")?;
+
+    let mut cmd = Command::cargo_bin("rnc")?;
+    cmd.arg("--dos2unix")
+        .arg(file.path());
+    cmd.assert()
+        .success();
+    let converted = fs::read(file)?;
+    assert_eq!(converted, b"foo\nbar\n");
+
+    Ok(())
+}
